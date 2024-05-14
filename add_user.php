32736f3fef5fd0d2c1c1b404dd030cfc9eb6374d
judercionhauche <judercionhauche@gmail.com>
2024-05-14 17:42:05 +0000

    initial commit

---
+++ add_user.php
@@ -0,0 +1,124 @@
+<?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+require_once('includes/load.php');
+
+// Retrieving all user groups from the database
+$groups = find_all('user_groups');
+?>
+
+<?php
+// Checking if form is submitted to add a new user
+if (isset($_POST['add_user'])) {
+
+  // Required fields for adding a user
+  $req_fields = array('first-name','last-name', 'username', 'password', 'level');
+  // Validating required fields
+  validate_fields($req_fields);
+
+  // Validating first name
+  if (isset($_POST['first-name']) && ctype_digit($_POST['first-name'])) {
+    $errors[] = "First name cannot consist solely of digits.";
+  }
+
+  // Validating last name
+  if (isset($_POST['last-name']) && ctype_digit($_POST['last-name'])) {
+    $errors[] = "Last name cannot consist solely of digits.";
+  }
+
+
+  // Validating username
+  if (!preg_match('/^\D.*$/', $_POST['username'])) {
+    $errors[] = "Username cannot be only digits.";
+  }
+
+  // Validating password using regex
+  if (!preg_match('/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/', $_POST['password'])) {
+    $errors[] = "Password must be at least 8 characters long and contain at least one lowercase letter, one uppercase letter, and one digit.";
+  }
+
+  // If there are no validation errors
+  if (empty($errors)) {
+    // Escaping and sanitizing input data
+    $first_name   = remove_junk($db->escape($_POST['first-name']));
+    $last_name = remove_junk($db->escape($_POST['last-name']));
+    $username   = remove_junk($db->escape($_POST['username']));
+    $password   = remove_junk($db->escape($_POST['password']));
+    $user_level = (int)$db->escape($_POST['level']);
+    // Hashing the password
+    $password = sha1($password);
+    // Constructing SQL query to add user
+    $query = "INSERT INTO users (";
+    $query .= "first_name,last_name,username,password,user_level,status";
+    $query .= ") VALUES (";
+    $query .= " '{$first_name}','{$last_name}', '{$username}', '{$password}', '{$user_level}','1'";
+    $query .= ")";
+    // Executing the query
+    if ($db->query($query)) {
+      // Success message
+      $session->msg('s', "User account has been created! ");
+      // Redirecting to login page
+      redirect('login_v2.php', false);
+    } else {
+      // Failure message
+      $session->msg('d', ' Sorry failed to create account!');
+      // Redirecting back to add user page
+      redirect('add_user.php', false);
+    }
+  } else {
+    // If there are validation errors, display error message
+    $session->msg("d", $errors);
+    // Redirecting back to add user page
+    redirect('add_user.php', false);
+  }
+}
+?>
+<?php include_once('layouts/header.php'); ?>
+<?php echo display_msg($msg); ?>
+<div class="row">
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <strong>
+        <span class="glyphicon glyphicon-th"></span>
+        <span>Add New User</span>
+      </strong>
+    </div>
+    <div class="panel-body">
+      <div class="col-md-6">
+        <form method="post" action="add_user.php">
+          <div class="form-group">
+            <label for="name">First Name</label>
+            <input type="text" class="form-control" name="first-name" placeholder="First Name" value="<?php echo isset($_POST['full-name']) ? htmlspecialchars($_POST['full-name']) : ''; ?>">
+          </div>
+          <div class="form-group">
+            <label for="name">Last Name</label>
+            <input type="text" class="form-control" name="last-name" placeholder="Last Name" value="<?php echo isset($_POST['last-name']) ? htmlspecialchars($_POST['last-name']) : ''; ?>">
+          </div>
+          <div class="form-group">
+            <label for="username">Username</label>
+            <input type="text" class="form-control" name="username" placeholder="Username" value="<?php echo isset($_POST['username']) ? htmlspecialchars($_POST['username']) : ''; ?>">
+          </div>
+          <div class="form-group">
+            <label for="password">Password</label>
+            <input type="password" class="form-control" name="password" placeholder="Password">
+          </div>
+          <div class="form-group">
+            <label for="level">User Role</label>
+            <select class="form-control" name="level">
+              <?php foreach ($groups as $group) : ?>
+                <option value="<?php echo $group['group_level']; ?>"><?php echo ucwords($group['group_name']); ?></option>
+              <?php endforeach; ?>
+            </select>
+          </div>
+          <div class="form-group clearfix">
+            <button type="submit" name="add_user" class="btn btn-primary">Add User</button>
+          </div>
+        </form>
+      </div>
+
+    </div>
+
+  </div>
+</div>
+
+<!-- <?php include_once('layouts/footer.php'); ?> -->
---
+++ includes/functions.php
@@ -0,0 +1,154 @@
+<?php
+ $errors = array();
+
+ /*--------------------------------------------------------------*/
+ /* Function for Remove escapes special
+ /* characters in a string for use in an SQL statement
+ /*--------------------------------------------------------------*/
+function real_escape($str){
+  global $con;
+  $escape = mysqli_real_escape_string($con,$str);
+  return $escape;
+}
+/*--------------------------------------------------------------*/
+/* Function for Remove html characters
+/*--------------------------------------------------------------*/
+// In includes/functions.php
+
+// Function to remove junk characters from a string or an array of strings
+function remove_junk($string) {
+  if(is_array($string)) {
+      $cleaned_strings = array();
+      foreach($string as $str) {
+          $cleaned_strings[] = trim($str); // You may want to adjust this based on your specific needs
+      }
+      return $cleaned_strings;
+  } else {
+      return trim($string); // You may want to adjust this based on your specific needs
+  }
+}
+
+/*--------------------------------------------------------------*/
+/* Function for Uppercase first character
+/*--------------------------------------------------------------*/
+function first_character($string) {
+  if(is_array($string)) {
+      $first_chars = array();
+      foreach($string as $str) {
+          $first_chars[] = ucfirst($str);
+      }
+      return $first_chars;
+  } else {
+      return ucfirst($string);
+  }
+}
+/*--------------------------------------------------------------*/
+/* Function for Checking input fields not empty
+/*--------------------------------------------------------------*/
+function validate_fields($var){
+  global $errors;
+  foreach ($var as $field) {
+    $val = remove_junk($_POST[$field]);
+    if(empty($val)){
+      $errors[] = ucfirst(str_replace('-', ' ', $field)) . " can't be blank.";
+    }
+  }
+}
+
+/*--------------------------------------------------------------*/
+/* Function for Display Session Message
+   Ex echo displayt_msg($message);
+/*--------------------------------------------------------------*/
+/**
+
+ * Display Messages
+ * @param array $msg
+ */
+function display_msg($msg = array()) {
+  $output = ''; // Initialize $output as a string
+  if(!empty($msg)) {
+     foreach ($msg as $key => $value) {
+        if (is_array($value)) {
+            foreach ($value as $val) {
+                $output .= "<div class=\"alert alert-{$key}\">"; // Concatenate the string with ".="
+                $output .= "<a href=\"#\" class=\"close\" data-dismiss=\"alert\">&times;</a>";
+                $output .= remove_junk(first_character($val));
+                $output .= "</div>";
+            }
+        } else {
+            $output .= "<div class=\"alert alert-{$key}\">"; // Concatenate the string with ".="
+            $output .= "<a href=\"#\" class=\"close\" data-dismiss=\"alert\">&times;</a>";
+            $output .= remove_junk(first_character($value));
+            $output .= "</div>";
+        }
+     }
+     return $output;
+  } else {
+    return ""; // Return an empty string if $msg is empty
+  }
+}
+
+
+
+/*--------------------------------------------------------------*/
+/* Function for redirect
+/*--------------------------------------------------------------*/
+function redirect($url, $permanent = false)
+{
+    if (headers_sent() === false)
+    {
+      header('Location: ' . $url, true, ($permanent === true) ? 301 : 302);
+    }
+
+    exit();
+}
+/*--------------------------------------------------------------*/
+/* Function for find out total saleing price, buying price and profit
+/*--------------------------------------------------------------*/
+function total_price($totals){
+   $sum = 0;
+   $sub = 0;
+   foreach($totals as $total ){
+     $sum += $total['total_saleing_price'];
+     $sub += $total['total_buying_price'];
+     $profit = $sum - $sub;
+   }
+   return array($sum,$profit);
+}
+/*--------------------------------------------------------------*/
+/* Function for Readable date time
+/*--------------------------------------------------------------*/
+function read_date($str){
+     if($str)
+      return date('F j, Y, g:i:s a', strtotime($str));
+     else
+      return null;
+  }
+/*--------------------------------------------------------------*/
+/* Function for  Readable Make date time
+/*--------------------------------------------------------------*/
+function make_date(){
+  return strftime("%Y-%m-%d %H:%M:%S", time());
+}
+/*--------------------------------------------------------------*/
+/* Function for  Readable date time
+/*--------------------------------------------------------------*/
+function count_id(){
+  static $count = 1;
+  return $count++;
+}
+/*--------------------------------------------------------------*/
+/* Function for Creating random string
+/*--------------------------------------------------------------*/
+function randString($length = 5)
+{
+  $str='';
+  $cha = "0123456789abcdefghijklmnopqrstuvwxyz";
+
+  for($x=0; $x<$length; $x++)
+   $str .= $cha[mt_rand(0,strlen($cha))];
+  return $str;
+}
+
+
+?>
