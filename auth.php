21aa203f5b709c8e55f652f850bdadd60d24dd31
judercionhauche <judercionhauche@gmail.com>
2024-08-01 12:12:44 +0000

    check active status

--- auth.php
+++ auth.php
@@ -8,27 +8,29 @@ $password = remove_junk($_POST['password']);
 if(empty($errors)){
   $user_id = authenticate($username, $password);
   if($user_id){
-    //create session with id
-     $session->login($user_id);
-     $_SESSION['username']= $username;
-    //Update Sign in time
-     updateLastLogIn($user_id);
-     $session->msg("s", "Welcome to Meditrack Management System");
-     redirect('home.php',false);
-
+    // Check user status
+    $query = "SELECT status FROM users WHERE id='{$user_id}'";
+    $result = $db->query($query);
+    $user = $db->fetch_assoc($result);
+    
+    if($user['status'] == '0'){
+      $session->msg("d", "This account is no longer active.");
+      redirect('index.php',false);
+    } else {
+      // Create session with id
+      $session->login($user_id);
+      $_SESSION['username']= $username;
+      // Update Sign in time
+      updateLastLogIn($user_id);
+      $session->msg("s", "Welcome to Meditrack Management System");
+      redirect('home.php',false);
+    }
   } else {
     $session->msg("d", "Sorry, Username/Password incorrect.");
     redirect('index.php',false);
   }
-
 } else {
    $session->msg("d", $errors);
    redirect('index.php',false);
 }
-
-
-// function to authenticate user
-//function to update last login of user
-//function to display message
-//function to redirect user
 ?>
